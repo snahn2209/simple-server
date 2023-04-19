4c26c3b79bb224993854c81ab467ea48f4034aa9
snahn2209 <sina.han@gmx.de>
2023-04-19 21:28:42 +0200

    added index.html file
    browser cannot find the html file, I can't find the mistake, there is no error but browser just keeps on loading the page...
    everything else works

--- main.js
+++ main.js
@@ -1,31 +1,42 @@
 const { listenerCount } = require("process");
 
-const routeResponseMap = {
+/*const routeResponseMap = {
     "/info": "<h1>Informationen</h1>",
     "/contact": "<h1>Kontakt</h1>",
     "/about": "<h1>Über uns</h1>",
     "/hello": "<h1>HI!</h1>",
     "/error": "<h1>Fehler</h1>",
-}
+}*/
 
 const port = 3000, 
     http = require("http"),
     httpStatus = require("http-status-codes"),
-    app = http.createServer((request, response) => {
-        console.log("Received an incoming request!");
+    fs = require("fs");
+const routeMap = {
+    "/": "views/index.html"
+};
+http
+    .createServer((request, response) => {
         response.writeHead(httpStatus.OK, {
             "Content-Type": "text/html"
         });
+        if (routeMap[request.url]){
+            fs.readFile(routeMap[request.url], (error, data) => {
+                response.write(data);
+                response.end;
+            });
+        } else {
+            response.end("<h1>sorry, not found.</h1>")
+        }
 
-        if (routeResponseMap[request.url]) {
+        /*if (routeResponseMap[request.url]) {
             response.end(routeResponseMap[request.url]);
         } else {
             response.end("<h1>Willkommen!</h1>");
-        }
+        }*/
 
-    });
-
-app.listen(port);
+    })    
+    .listen(port);
 console.log('The server has started and is listening on port number: ' + port);
 
 //sprint_01
