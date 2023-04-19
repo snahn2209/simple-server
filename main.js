d46ea8d04674c80d58a39d86e854607882d05fe9
snahn2209 <sina.han@gmx.de>
2023-04-19 22:03:19 +0200

    added more html files

--- main.js
+++ main.js
@@ -12,9 +12,7 @@ const port = 3000,
     http = require("http"),
     httpStatus = require("http-status-codes"),
     fs = require("fs");
-/*const routeMap = {
-    "/": "views/index.html"
-};*/
+
 const getViewUrl = (url) => {
     return 'views/'+url+'.html';
 };
