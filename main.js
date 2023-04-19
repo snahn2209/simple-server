db1dd4acf8f64441199322692839026babe04a1e
snahn2209 <sina.han@gmx.de>
2023-04-19 22:47:05 +0200

    added specific routes for each file in project

--- main.js
+++ main.js
@@ -1,40 +1,72 @@
 const { listenerCount } = require("process");
 
-/*const routeResponseMap = {
-    "/info": "<h1>Informationen</h1>",
-    "/contact": "<h1>Kontakt</h1>",
-    "/about": "<h1>Über uns</h1>",
-    "/hello": "<h1>HI!</h1>",
-    "/error": "<h1>Fehler</h1>",
-}*/
+
 
 const port = 3000, 
     http = require("http"),
     httpStatus = require("http-status-codes"),
     fs = require("fs");
 
-const getViewUrl = (url) => {
-    return 'views/'+url+'.html';
+const sendErrorResponse = res => {
+    res.writeHead(httpStatus.NOT_FOUND, {
+        "Content-Type": "text/html"
+    });
+    res.write("<h1>FILE WAS NOT FOUND </h1>");
+    res.end;
 };
+
+/*const getViewUrl = (url) => {
+    return 'views/'+url+'.html';
+};*/
 
 http
     .createServer((req, res) => {
-        let viewUrl = getViewUrl(req.url);
-        fs.readFile(viewUrl, (error, data) => {
-            if(error) {
-                res.writeHead(httpStatus.NOT_FOUND);
-                res.write("<h1>FILE WAS NOT FOUND </h1>")
-            } else {
-                res.writeHead(httpStatus.OK, {
-                    "Content-Type": "text/html"
-                });
-                res.write(data);
-            }
-            res.end();
+        let url = req.url;
+        if (url.indexOf(".html") !== -1) {
+            res.writeHead(httpStatus.OK, {
+                "Content-Type": "text/html"
         });
+        customReadFile('./views/' + url, res);
+
+        } else if (url.indexOf(".js") !== -1) {
+            res.writeHead(httpStatus.OK, {
+            "Content-Type": "text/javascript"
+        });
+        customReadFile('./public/js/' + url, res);
+
+        } else if (url.indexOf(".css") !== -1) {
+            res.writeHead(httpStatus.OK, {
+            "Content-Type": "text/css"
+        });
+        customReadFile('./public/css/' + url, res);
+
+        } else if (url.indexOf(".png") !== -1) {
+            res.writeHead(httpStatus.OK, {
+            "Content-Type": "image/png"
+        });
+        customReadFile('./public/images/' + url, res);
+        } else {
+            sendErrorResponse(res);
+        }
 
     })    
     .listen(port);
 console.log('The server has started and is listening on port number: ' + port);
+
+const customReadFile = (file_path, res) => {
+    if (fs.existsSync(file_path)){
+        fs.readFile(file_path, (error, data) => {
+            if (error) {
+                console.log(error);
+                sendErrorResponse(res);
+                return;
+            }
+            res.write(data);
+            res.end();
+        })
+    } else 
+        sendErrorResponse(res);
+    
+};
 
 //sprint_01
